677884ffc20c488ef600c031bf0843a4cf0d744a
Sönke Erfkamp <soenke.erfkamp@tum.de>
2018-09-07 02:05:46 +0200

    full screen and immersive mode

--- app/src/main/java/com/example/serfk/myapplication/IVISActivity.java
+++ app/src/main/java/com/example/serfk/myapplication/IVISActivity.java
@@ -13,6 +13,8 @@ import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.Transformation;
 import android.widget.ImageView;
@@ -66,6 +68,21 @@ public class IVISActivity extends AppCompatActivity implements View.OnTouchListe
     private int lockingDuration;
 
     private boolean isInteracting = false;
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+
+        if (hasFocus) {
+            getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
